6367cbc1dacaa690688506dc447cb12fcf8891fd
oytUser <oyt201@naver.com>
2026-05-19 13:31:30 +0900

    프로필 라우터 추가

--- app/api/api.py
+++ app/api/api.py
@@ -6,3 +6,5 @@ api_router.include_router(system.router, tags=["System"])
 api_router.include_router(user.router, prefix="/users", tags=["Users"])
 api_router.include_router(movie.router, prefix="/movies", tags=["Movies"])
 api_router.include_router(test.router, prefix="/test", tags=["Tests"])
+
+api_router.include_router(profile.router, prefix="/profile", tags=["Persona"])
