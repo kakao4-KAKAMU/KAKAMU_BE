e3e9e3788226e558a25d5b5e304643c3bbc4aa53
oyt2020 <oyt201@naver.com>
2026-05-20 22:41:34 +0900

    업데이트 엔드포인트 완성, redis 추가

--- app/api/endpoints/profile/update_persona.py
+++ app/api/endpoints/profile/update_persona.py
@@ -6,7 +6,8 @@ from app.api.deps import get_current_user
 from app.db.session import get_db
 from app.schemas.profile import PersonaEdit
 from app.models.models import User
-from app.service.persona import PersonaService
+from app.service.profile import PersonaService
+from app.core.redis import get_redis
 
 router = APIRouter()
 
@@ -15,6 +16,7 @@ def edit_persona_profile(
         persona_id: int,
         persona_edit_data: PersonaEdit,
         user: User = Depends(get_current_user),
-    db: Session = Depends(get_db)
+        db: Session = Depends(get_db),
+        redis_client = Depends(get_redis)
 ):
-    return PersonaService.update_persona(db=db, persona_id=persona_id,edit_data=persona_edit_data,user_id=user.id)
+    return PersonaService.update_persona(db=db, redis_client=redis_client, persona_id=persona_id,edit_data=persona_edit_data,user_id=user.id)
