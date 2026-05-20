db5c5ac3f32cdb6e519c564a378538094bec5b3e
oyt2020 <oyt201@naver.com>
2026-05-20 22:42:24 +0900

    ã페르소나 조회 로직 완성

--- app/service/profile/read_persona.py
+++ app/service/profile/read_persona.py
@@ -1,41 +1,73 @@
 import redis
 
-from app.core.redis import redis_client
 import re
 from fastapi import HTTPException, status
 from sqlalchemy import select, and_, func, update
 from sqlalchemy.orm import Session
 from app.models.models import Persona
 from app.schemas.profile import PersonaCreate
+from typing import List
 
 class PersonaReadService:
 
-    async def activate_persona(db: Session,persona_id: id, user_id: int):
-        db_persona = db.get(Persona,persona_id)
-        THREE_DAY = 259200 # 3일 동안 활성화
-        if not db_persona or db_persona.user_id != user_id:
-            return False
+    # 내 모든 페르소나 조회
+    @staticmethod
+    async def get_my_personas(db: Session, user_id: int) -> List[Persona]:
+        stmt = select(Persona).where(
+            Persona.user_id == user_id,
+            Persona.status != "DELETED"
+        )
 
-        try: # 해당 유저의 모든 페르소나를 off로 전환하고
-            db.execute(
-                update(Persona).where(Persona.id == persona_id).values(preference_status="off")
+        return list(db.scalars(stmt).all())
+
+    # 특정 페르소나 조회
+    @staticmethod
+    async def get_persona_detail(
+        db: Session,
+        user_id: int,
+        persona_id: int
+    ) -> Persona:
+        stmt = select(Persona).where(
+            Persona.id == persona_id,
+            Persona.user_id == user_id,
+            Persona.status != "DELETED"
+        )
+
+        persona = db.scalar(stmt)
+
+        if not persona:
+            raise HTTPException(
+                status_code=404,
+                detail="페르소나를 찾을 수 없습니다."
             )
-            db_persona.preference_status = "on" # 선택한 페르소나만 on으로 전환
-            db.commit()
 
-            redis_key = f"kakamu:user:{user_id}:current_persona"
+        return persona
 
-            await redis_client.set(redis_key,persona_id, ex=THREE_DAY) # 3일동안 유지
+    # 현재 활성화 된 페르소나 계정 조회
+    @staticmethod
+    async def get_current_persona(db: Session, redis_client, user_id: int):
+        persona_id = await PersonaReadService.get_current_active_persona_id(
+            db=db,
+            redis_client=redis_client,
+            user_id=user_id
+        )
 
-        except Exception as e:
-            db.rollback()
-            return False
+        if not persona_id:
+            raise HTTPException(
+                status_code=400,
+                detail="활성화된 페르소나가 없습니다."
+            )
 
+        return await PersonaReadService.get_persona_detail(
+            db=db,
+            user_id=user_id,
+            persona_id=persona_id
+        )
 
 
     # 현재 활성화 된 계정 가져오기 + 자동 기간 갱신
     @staticmethod
-    async def get_current_active_persona_id(db: Session, user_id: int) -> int:
+    async def get_current_active_persona_id(db: Session,redis_client, user_id: int) -> int:
         redis_key = f"kakamu:user:{user_id}:current_persona"
         THREE_DAY = 259200
 
@@ -47,8 +79,8 @@ class PersonaReadService:
             await redis_client.expire(redis_key, THREE_DAY)
             return int(cached_persona_id)
 
-        # 만약 없으면 현재 "on" 상태의 페르소나 조회
-        stmt = select(Persona).where(
+        # 만약 없으면 현재 "on" 상태의 페르소나 조회 후 페르소나 id만 조회
+        stmt = select(Persona.id).where(
             Persona.user_id == user_id,
             Persona.preference_status == "on"
         )
