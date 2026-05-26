5e5f354f609df16340b24b32d9c2f65242bd21dc
oytUser <oyt201@naver.com>
2026-05-26 12:48:28 +0900

    로그 처리 미들웨어 추가

--- app/main.py
+++ app/main.py
@@ -12,6 +12,8 @@ from app.core.config import settings
 from app.core.redis import redis_client
 from app.api.api import api_router
 
+from app.middleware.logging_middleware import LoggingMiddleware
+
 def create_database_if_not_exists():
     """데이터베이스가 존재하지 않으면 생성합니다."""
     db_url = settings.DATABASE_URL
@@ -92,6 +94,8 @@ app.add_middleware(
     allow_methods=["*"],  # GET, POST, PUT, DELETE 등 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 HTTP 헤더 허용
 )
+# 로그 미들웨어 등록
+app.add_middleware(LoggingMiddleware)
 
 # 중앙 라우터 허브 등록
 app.include_router(api_router)
