df7adf504d8875191f4ec2fef9339866962efe40
oytUser <oyt201@naver.com>
2026-05-26 12:48:09 +0900

    페르소나 삭제 로직, 엔드포인트 작성

--- app/api/endpoints/profile/delete_persona.py
+++ app/api/endpoints/profile/delete_persona.py
@@ -2,6 +2,7 @@ import redis
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
+from app.models.models import User
 from app.api.deps import get_current_user
 from app.core.redis import get_redis
 from app.db.session import get_db
@@ -12,6 +13,16 @@ from app.service.profile import PersonaService
 router = APIRouter()
 
 @router.delete("/persona/{persona_id}", status_code=204)
-def delete_persona(
+async def delete_persona(
+        persona_id: int,
+        user:User = Depends(get_current_user),
+        db: Session = Depends(get_db),
+        redis_client : redis.Redis = Depends(get_redis)
 ):
+    await PersonaService.delete_persona_soft(
+        db = db,
+        redis_client = redis_client,
+        user_id = user.id,
+        persona_id = persona_id
+    )
     return None
