6960ea9089f2af07226c914951b72b07dab2c3c6
oytUser <oyt201@naver.com>
2026-05-28 16:30:08 +0900

    tracer 수집 로컬, 클라우드 환경 분리

--- app/core/tracing.py
+++ app/core/tracing.py
@@ -4,7 +4,7 @@ import os
 from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, SimpleSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
@@ -16,7 +16,7 @@ from app.db.session import engine
 
 def setup_tracing(app):
     service_name = os.getenv("OTEL_SERVICE_NAME", "filma-back")
-    environment = os.getenv("APP_ENV", "dev")
+    environment = os.getenv("APP_ENV", "local")
     otlp_endpoint = os.getenv(
         "OTEL_EXPORTER_OTLP_ENDPOINT",
         "alloy.monitoring.svc.cluster.local:4317"
@@ -30,21 +30,25 @@ def setup_tracing(app):
     provider = TracerProvider(resource=resource)
     trace.set_tracer_provider(provider)
 
-    # 수집한 trace 데이터를 어디로 보낼지 결정
-    otlp_exporter = OTLPSpanExporter(
-        endpoint=otlp_endpoint, # 쿠버네티스 내부 dns 주소
-        insecure=True, # 평문으로 보내겠다 (내부 클러스터 통신)
-    )
+    is_local = environment == "local"
+    is_console_true = os.getenv("OTEL_CONSOLE_EXPORTER", "false").lower() == "true"
 
-    # 생성된 span을 바로 안 보내고 모아서 배치로 전송
-    provider.add_span_processor(
-        BatchSpanProcessor(otlp_exporter)
-    )
-
-    # 로컬로 하고 싶으면 설정 파일에 true으로 해야 함.
-    if os.getenv("OTEL_CONSOLE_EXPORTER", "false").lower() == "true":
+    if is_local or is_console_true:
+        print("로컬 환경")
         provider.add_span_processor(
-            BatchSpanProcessor(ConsoleSpanExporter())
+            SimpleSpanProcessor(ConsoleSpanExporter())
+        )
+
+    if not is_local:
+        # 수집한 trace 데이터를 어디로 보낼지 결정
+        otlp_exporter = OTLPSpanExporter(
+            endpoint=otlp_endpoint, # 쿠버네티스 내부 dns 주소
+            insecure=True, # 평문으로 보내겠다 (내부 클러스터 통신)
+        )
+
+        # 생성된 span을 바로 안 보내고 모아서 배치로 전송
+        provider.add_span_processor(
+            BatchSpanProcessor(otlp_exporter)
         )
 
     # 중복 출력 방지
