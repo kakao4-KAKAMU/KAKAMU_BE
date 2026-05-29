6969df2b868c85a175b7a8c69c34ba4877aa84fd
oytUser <oyt201@naver.com>
2026-05-29 17:33:33 +0900

    롤백

--- app/api/endpoints/profile/create_persona.py
+++ app/api/endpoints/profile/create_persona.py
@@ -14,26 +14,13 @@ router = APIRouter()
 
 @router.post("/persona", response_model=PersonaResponse, status_code=201)
 async def new_persona_profile(
-        nickname: str = Form(...),
-        profile_msg: str = Form(""),
-        fav_movie_ids: Optional[List[int]] = Form(None),
-        fav_genre_ids: Optional[List[int]] = Form(None),
-        fav_people_ids: Optional[List[int]] = Form(None),
-        profile_image: Optional[UploadFile] = File(None),
+        persona_data: PersonaCreate,
 
 
         user: User = Depends(get_current_user), # 현재 액세스 토큰으로 인증된 user 정보
         db: Session = Depends(get_db) # db 연결 후 세션 객체
 ):
-    persona_data = PersonaCreate(
-        nickname=nickname,
-        profile_msg=profile_msg,
-        fav_movie_ids=fav_movie_ids,
-        fav_genre_ids=fav_genre_ids,
-        fav_people_ids=fav_people_ids,
-    )
 
     return await PersonaService.create_new_persona(db=db,
                                                    persona_data=persona_data,
-                                                   user_id=user.id,
-                                                   profile_image=profile_image)
+                                                   user_id=user.id)
