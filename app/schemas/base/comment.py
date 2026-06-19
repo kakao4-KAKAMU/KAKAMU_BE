ab788979dacfefbb5a3a962160bf570e5a309139
cjhih456 <cjhih43@gmail.com>
2026-06-19 14:43:30 +0900

    add: comment hashtags & mentions

--- app/schemas/base/comment.py
+++ app/schemas/base/comment.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 
 from pydantic import BaseModel
 
+from app.schemas.base.mention import Mention
 from app.schemas.base.user import UserSimple
 
 
@@ -15,4 +16,6 @@ class CommentItem(BaseModel):
     created_at: datetime
     like_count: int
     is_liked: bool
+    hashtags: List[str] = []
+    mentions: List[Mention] = []
 
--- app/schemas/mapper/comment.py
+++ app/schemas/mapper/comment.py
@@ -1,9 +1,12 @@
-from typing import Optional
+from typing import List, Optional
 
 from app.models import Comment
 from app.models.user import User as UserModel
 from app.schemas.base.comment import CommentItem
+from app.schemas.base.mention import Mention
 from app.schemas.mapper.user import UserMapper
+
+SPOILER_MASK = "*** 스포일러로 인해 블라인드 처리되었습니다. 보기 버튼을 눌러 확인하세요. ***"
 
 
 class CommentMapper:
@@ -12,15 +15,22 @@ class CommentMapper:
         comment: Comment,
         author: Optional[UserModel],
         *,
+        hashtags: Optional[List[str]] = None,
+        mentions: Optional[List[Mention]] = None,
         is_liked: bool = False,
+        mask_spoiler: bool = True,
     ) -> CommentItem:
+        is_spoiler = comment.is_spoiler == 1
+        content = SPOILER_MASK if mask_spoiler and is_spoiler else comment.content
         return CommentItem(
             id=comment.id,
             parent_id=comment.parent_id,
             user=UserMapper.to_simple_or_anonymous(author),
-            content=comment.content,
-            is_spoiler=comment.is_spoiler == 1,
+            content=content,
+            is_spoiler=is_spoiler,
             created_at=comment.created_at,
             like_count=comment.like_count,
             is_liked=is_liked,
+            hashtags=hashtags or [],
+            mentions=mentions or [],
         )
