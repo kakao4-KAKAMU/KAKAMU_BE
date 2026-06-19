b0795689717a2b61e80cdcb61d93bb891675a3c8
cjhih456 <cjhih43@gmail.com>
2026-06-19 22:32:58 +0900

    fix: search person on eng

--- app/service/search/search_service.py
+++ app/service/search/search_service.py
@@ -46,7 +46,12 @@ class SearchService:
     ) -> PersonFilterSearchResponse:
         query = db.query(People)
         if search_pattern:
-            query = query.filter(People.person_name.ilike(search_pattern))
+            query = query.filter(
+                or_(
+                    People.person_name.ilike(search_pattern),
+                    People.person_name_eng.ilike(search_pattern),
+                )
+            )
         if jobs:
             query = query.join(MovieStaff, People.id == MovieStaff.people_id).filter(MovieStaff.job.in_(jobs))
 
