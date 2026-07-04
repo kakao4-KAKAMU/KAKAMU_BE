f1c88af6f296dee9fad2c235495a01601d135532
cjhih456 <cjhih43@gmail.com>
2026-07-04 16:52:55 +0900

    fix: missing class

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,7 +3,15 @@ from .persona import Persona, PersonaStatus, FavGenre, FavPeople, FavMovie
 from .relation import Follow, Block, BlockLevel
 from .post import Post, PostStatus, Hashtag, PostHashtag, PostMention, PostMovie
 from .movie import Movie, Genre, People, MovieGenre, MovieStaff, MovieEvaluation, MovieOriginalTitle, YoutubeVideo
-from .activity import Comment, CommentMention, CommentHashtag, LikeLog, SaveLog, SemanticAnalysis
+from .activity import (
+    Comment,
+    CommentStatus,
+    CommentMention,
+    CommentHashtag,
+    LikeLog,
+    SaveLog,
+    SemanticAnalysis,
+)
 from .search_log import SearchLog, SearchDailyStat
 from .notification import Notification, NotificationType
 from .ml import ChatMetadataType, JudgeType
